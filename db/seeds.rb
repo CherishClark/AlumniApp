38854013700137ba2ddc3deb774ab31a755faaac
JustimusPrime <hirejustin@yahoo.com>
2018-01-20 15:22:54 -0700

    Implemented kaminari for paginate, updated seeds file, formated home page to display the posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+
+100.times do 
+
+  Post.create(title: "OH. BOY", ranking: rand(1..10), topic: "stuff", comment: "comment", images: "http://via.placeholder.com/150x150", body: "awwwwwwwwwwwww snaaaaaaaaaapppppppppppppppp")
+
+end
+
+puts "made 100,000,000 posts y'all"
