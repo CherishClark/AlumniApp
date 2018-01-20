aed7c41c0fcd46834259a465f5539fd272c1732b
Adam <a_prusse@hotmail.com>
2018-01-20 16:07:30 -0700

    Implemented topic option

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+validates_presence_of :title, :body
+
+belongs_to :topic
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180120183741) do
-
-
-
-
-
-
-
+ActiveRecord::Schema.define(version: 20180120213317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +23,14 @@ ActiveRecord::Schema.define(version: 20180120183741) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
+    t.bigint "topic_id"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20180120183741) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
+  add_foreign_key "posts", "topics"
 end
