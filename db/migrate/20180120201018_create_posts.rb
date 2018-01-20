c1bca9212764c8ce7798fd17ca8ff088819e8304
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-20 14:29:27 -0600

    thanks Andrew

--- db/migrate/20180120201018_create_posts.rb
+++ db/migrate/20180120201018_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.integer :ranking
       t.string :topic
+      t.text :body
       t.text :comment
       t.text :images
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120183741) do
+ActiveRecord::Schema.define(version: 20180120201018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 20180120183741) do
   create_table "posts", force: :cascade do |t|
     t.integer "ranking"
     t.string "topic"
+    t.text "body"
     t.text "comment"
     t.text "images"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
