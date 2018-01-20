69a53d8b3b26ecc76409782e675d6bda2d611301
Chris Cook <cdani3l.cook@gmail.com>
2018-01-20 11:39:02 -0700

    added body text

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180120183741) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "ranking"
+    t.string "topic"
+    t.text "comment"
+    t.text "images"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body"
+  end
 
 end
