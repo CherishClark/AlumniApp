7ddd0f6a14f524e2c1ee8e96a3a7826f8438c54e
Bryan Lund <bryan.helge@gmail.com>
2018-01-20 17:20:10 -0700

    Added votes to seeds and get_featured_posts to post controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+@user = User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
 
 
 100.times do 
 
-  Post.create(title: "OH. BOY", ranking: rand(1..10), topic: "stuff", comment: "comment", images: "http://via.placeholder.com/150x150", body: "awwwwwwwwwwwww snaaaaaaaaaapppppppppppppppp")
+  post = Post.create(title: "OH. BOY", ranking: rand(1..10), topic: "stuff", comment: "comment", images: "http://via.placeholder.com/150x150", body: "awwwwwwwwwwwww snaaaaaaaaaapppppppppppppppp")
+
+	[*1..15].sample.times do
+	  post.upvote_by @user
+	end
 
 end
 
