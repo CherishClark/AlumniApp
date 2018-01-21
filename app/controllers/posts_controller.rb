5cb17ab3ac103c7781fa5d9908b7e63c9be2e345
Tania D'Angelo <tania.a.dangelo@outlook.com>
2018-01-21 15:03:21 -0600

    Changed seeds file to include less posts and worked on comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -86,6 +86,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:ranking, :topic, :body, :title, :comment, :images)
+      params.require(:post).permit(:ranking, :topic, :body, :title, :images)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
 
 
-100.times do 
+10.times do 
 
-  Post.create(title: "OH. BOY", ranking: rand(1..10), topic: "stuff", comment: "comment", images: "http://via.placeholder.com/150x150", body: "awwwwwwwwwwwww snaaaaaaaaaapppppppppppppppp")
+  Post.create!(title: "OH. BOY", ranking: rand(1..10), topic: "stuff", comment: "comment", images: "http://via.placeholder.com/150x150", body: "awwwwwwwwwwwww snaaaaaaaaaapppppppppppppppp")
 
 end
 
